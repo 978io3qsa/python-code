a72f403368a6186825f32b6bf9f45f8037ca8192
kyle <w_w_ming@hotmail.com>
2018-09-25 23:42:53 +0800

    爬虫-吃

--- aliyun-mon/test.py
+++ aliyun-mon/test.py
@@ -2,9 +2,13 @@
 
 from aliyunsdkcore import client
 from aliyunsdkcms.request.v20180308 import QueryMetricListRequest
+from openpyxl import Workbook
 import time
 import json
 import getInstances
+import datetime
+
+
 
 
 
@@ -12,41 +16,46 @@ ak, sk, domain = getInstances.getAkSkDomain('aliyun-mon/config.ini')
 
 ecs_dict = getInstances.getECSList('aliyun-mon/config.ini')
 
-print ak,sk,domain
-print ecs_dict
+clt = client.AcsClient(ak, sk, domain)
 
-'''
+def getDiskUsage(start_time, end_time, Period):
+    request = QueryMetricListRequest.QueryMetricListRequest()
+    request.set_accept_format('json')
+    request.set_Project('acs_ecs_dashboard')
+    request.set_Metric('diskusage_utilization')
 
-clt = client.AcsClient(
-    "LTAIry88qsasWzSD", 
-    "4A4Zzz2DtdgaU6xplhtq7NkgRUMFAs",
-    "cn-shenzhen")
-request = QueryMetricListRequest.QueryMetricListRequest()
-request.set_accept_format('json')
-request.set_Project('acs_ecs_dashboard')
-request.set_Metric('diskusage_utilization')
-start_time = "2018-08-01"
-timestamp_start = int(time.mktime(time.strptime(start_time, "%Y-%m-%d"))) * 1000
-
-end_time = "2018-09-01"
-timestamp_end = int(time.mktime(time.strptime(end_time, "%Y-%m-%d"))) * 1000
-
-request.set_StartTime(timestamp_start)
-request.set_EndTime(timestamp_end)
-request.set_Dimensions("{'instanceId':'i-wz94obs9dvxg9zu1n2s3'}")
-request.set_Period('86400')
-result = clt.do_action_with_exception(request)
-
-deal_result = json.loads(result)
-
-end_result =  json.loads(deal_result['Datapoints'])
-
-print len(end_result)
-
-for item in end_result:
-    if item['diskname'] == '/':
-        print(u'根分区磁盘使用率： %.2f' %item['Average']) 
+    timestamp_start = int(time.mktime(time.strptime(start_time,"%Y-%m-%d"))) * 1000
+    timestamp_end = int(time.mktime(time.strptime(end_time,"%Y-%m-%d"))) * 1000
     
-    
+    request.set_StartTime(timestamp_start)
+    request.set_EndTime(timestamp_end)
 
-   '''
+    rootUsage = []
+    appUsage = []
+    resultList = []
+
+    request.set_Period(Period)
+    for item in ecs_dict.values():
+        rootUsage.append(item)
+        appUsage.append(item)
+        request.set_Dimensions("{'instanceId': '" + item + "'}")
+        data = clt.do_action_with_exception(request)
+        result = json.loads(json.loads(data)['Datapoints'])
+        for values in result:
+            if values['diskname'] == '/' :
+                rootUsage.append(round(values['Average'],2))
+            if values['diskname'] == '/app' :
+                appUsage.append(round(values['Average'],2))
+        resultList.append(rootUsage)
+        resultList.append(appUsage)
+        rootUsage = []
+        appUsage = []
+
+    return resultList
+
+
+if __name__ == '__main__':
+   wb = Workbook()
+   ws = wb.active
+   ws.append(getDiskUsage('2018-09-18','2018-09-24','86400'))
+   wb.save("diskUsage.xlsx")
